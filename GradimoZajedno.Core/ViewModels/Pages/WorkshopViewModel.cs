75cd7d6ed04f28133ff1a44a75ada2f45fadc00a
Molee <milosmoskic147@gmail.com>
2022-10-15 16:14:07 +0200

    add workshop page

---
+++ GradimoZajedno.Core/ViewModels/Pages/WorkshopViewModel.cs
@@ -0,0 +1,25 @@
+namespace GradimoZajedno.Core.ViewModels.Pages;
+
+using GradimoZajedno.Core.Contexts;
+using GradimoZajedno.Core.Extensions;
+using GradimoZajedno.Core.ViewModels.Shared;
+using GradimoZajedno.Models.Generated;
+
+public class WorkshopViewModel : PageViewModel
+{
+    public WorkshopViewModel(IPageContext<Workshop> context) : base(context)
+    {
+        this.WorkshopTitle = context.Page.WorkshopTitle;
+        this.WorkshopDate = context.Page.WorkshopDate;
+        this.WorkshopText = context.Page.WorkshopText;
+        this.Picture1 = context.Page.WorkshopPicture1?.Content.TryCreateImageViewModel();
+        this.Picture2 = context.Page.WorkshopPicture2?.Content.TryCreateImageViewModel();
+    }
+
+    public string WorkshopTitle {get;set;}
+    public string WorkshopDate {get;set;}
+    public string WorkshopText {get;set;}
+
+    public ImageViewModel Picture1 {get;set;}
+    public ImageViewModel Picture2 {get;set;}
+}
