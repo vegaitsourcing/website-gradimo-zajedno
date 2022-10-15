94f58dc4f584729f0d03211dc902eb4581c935bc
Antonela Parovic <antonela.parovic@vegait.rs>
2022-10-15 12:41:12 +0200

    Add Header.cshtml

--- GradimoZajedno.Core/ViewModels/Partials/Layout/HeaderViewModel.cs
+++ GradimoZajedno.Core/ViewModels/Partials/Layout/HeaderViewModel.cs
@@ -2,6 +2,7 @@ namespace GradimoZajedno.Core.ViewModels.Partials.Layout;
 
 using System;
 using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Models;
 using GradimoZajedno.Core.Extensions;
 using GradimoZajedno.Core.ViewModels.Shared;
 using GradimoZajedno.Models.Generated;
@@ -15,8 +16,12 @@ public class HeaderViewModel
         Logo = header.Logo?.Content.TryCreateImageViewModel();
         var homeNode = ((PublishedContentModel)header).AncestorOrSelf<Home>();
         LogoUrl = homeNode.Url();
+
+        NavigationMenu = header.NavigationMenu.Select(link => link.ToViewModel());
+
     }
 
     public ImageViewModel Logo { get; }
     public string LogoUrl { get; }
+    public IEnumerable<LinkViewModel> NavigationMenu { get; }
 }
