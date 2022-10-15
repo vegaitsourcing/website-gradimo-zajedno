99604282614c40df82186ad47c23a2e18c4de666
Antonela Parovic <antonela.parovic@vegait.rs>
2022-10-15 15:43:22 +0200

    Add Footer.cshtml

--- GradimoZajedno.Core/ViewModels/Partials/Layout/FooterViewModel.cs
+++ GradimoZajedno.Core/ViewModels/Partials/Layout/FooterViewModel.cs
@@ -2,6 +2,10 @@ namespace GradimoZajedno.Core.ViewModels.Partials.Layout;
 
 using System;
 using GradimoZajedno.Models.Generated;
+using Umbraco.Cms.Core.Models;
+using GradimoZajedno.Core.Extensions;
+using GradimoZajedno.Core.ViewModels.Shared;
+
 
 public class FooterViewModel
 {
@@ -9,5 +13,16 @@ public class FooterViewModel
     {
         if (footer == null) throw new ArgumentNullException(nameof(footer));
 
+        Address = footer.Address;
+        Email = footer.Email;
+        MenuFirstColumn = footer.MenuFirstColumn.Select(link => link.ToViewModel());
+        MenuSecondColumn = footer.MenuSecondColumn.Select(link => link.ToViewModel());
+        Donations = footer.Donations;
     }
+
+    public String Address { get; }
+    public String Email { get; }
+    public IEnumerable<LinkViewModel> MenuFirstColumn { get; }
+    public IEnumerable<LinkViewModel> MenuSecondColumn { get; }
+    public String Donations { get; }
 }
