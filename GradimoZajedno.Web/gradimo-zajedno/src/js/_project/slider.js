09cdd44680c024abfb67d660dc5c40879a24a4f9
Petra Markovic <petra.markovic@vegait.rs>
2022-10-16 14:47:55 +0200

    bugs fix: slider on homepage & add missing popups on home and time capsule page

--- GradimoZajedno.Web/gradimo-zajedno/src/js/_project/slider.js
+++ GradimoZajedno.Web/gradimo-zajedno/src/js/_project/slider.js
@@ -2,32 +2,55 @@ import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/core';
 
 const slider = {
-	selector: '.js-slider',
+	options: {},
 
 	init: function() {
 		this.swiper();
 	},
 
 	swiper: function() {
+		if (Array.from(document.querySelectorAll('.js-swiper .swiper-slide')).length === 1) {
+			this.options = {
+				loop: false,
+				spaceBetween: 20,
+				slidesPerView: 1,
+
+				// If we need pagination
+				pagination: {
+					el: '.swiper-pagination',
+				},
+
+				// Navigation arrows
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev'
+				},
+
+				modules: [Navigation, Pagination]
+			};
+		} else {
+			this.options = {
+				loop: true,
+				spaceBetween: 20,
+				slidesPerView: 1,
+				loopedSlides: 2,
+
+				// If we need pagination
+				pagination: {
+					el: '.swiper-pagination',
+				},
+
+				// Navigation arrows
+				navigation: {
+					nextEl: '.swiper-button-next',
+					prevEl: '.swiper-button-prev'
+				},
+
+				modules: [Navigation, Pagination]
+			};
+		}
 		// eslint-disable-next-line no-unused-vars
-		const swiper = new Swiper('.js-swiper', {
-			// Optional parameters
-			loop: true,
-			spaceBetween: 20,
-
-			// If we need pagination
-			pagination: {
-				el: '.swiper-pagination',
-			},
-
-			// Navigation arrows
-			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev'
-			},
-
-			modules: [Navigation, Pagination]
-		});
+		const swiper = new Swiper('.js-swiper', this.options);
 	}
 };
 
