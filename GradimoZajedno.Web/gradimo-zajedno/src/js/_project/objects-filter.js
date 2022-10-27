81e21793432d2dbd1130619d88b67bafe5bdbb22
Igor Gajic <i.gajic@vegait.rs>
2022-10-27 12:28:01 +0200

    Fixed filtering by quarter and object type

--- GradimoZajedno.Web/gradimo-zajedno/src/js/_project/objects-filter.js
+++ GradimoZajedno.Web/gradimo-zajedno/src/js/_project/objects-filter.js
@@ -14,33 +14,52 @@ const objectsFilter = {
 	},
 
 	onButtonClick: function(e) {
-		const filterButtons = document.querySelectorAll('.js-obejcts-filter-button');
-		filterButtons.forEach((button) => button.classList.remove('filter__label--checked'));
+		const filterButtons = [...document.querySelectorAll('.js-objects-bottom-filter-row .js-obejcts-filter-button')];
+		const quarterButtons = [...document.querySelectorAll('.js-objects-top-filter-row .js-obejcts-filter-button')];
 		const button = e.target;
-		const buttonAttribute = e.target.getAttribute('data-name');
-		const items = document.querySelectorAll('.js-object-item-cont');
+
+		if (filterButtons.includes(button)) {
+			filterButtons.forEach((button) => button.classList.remove('filter__label--checked'));
+		} else {
+			quarterButtons.forEach((button) => button.classList.remove('filter__label--checked'));
+			filterButtons.forEach((button) => button.classList.remove('filter__label--checked'));
+		}
 		button.classList.add('filter__label--checked');
-		items.forEach((item, index) => {
-			if (buttonAttribute === '') {
-				return item.classList.remove('sr-only', 'active-item');
+
+		let activeQuarter = null;
+		for (const b of quarterButtons) {
+			if (b.classList.contains('filter__label--checked')) {
+				activeQuarter = b.getAttribute('data-name');
+			}
+		}
+
+		const activeFilter = e.target.getAttribute('data-name');
+		const allItems = document.querySelectorAll('.js-object-item-cont');
+
+		allItems.forEach((item) => {
+			if (activeFilter === '') {
+				item.classList.remove('sr-only', 'active-item');
 			}
 
 			const itemTags = item.getAttribute('data-tags').split(',');
 
-			if (itemTags.find((el) => el === buttonAttribute)) {
-				return item.classList.remove('sr-only');
+			if (itemTags.find((tag) => tag === activeFilter || activeFilter === '')) {
+				if (activeQuarter === null || activeQuarter !== null && itemTags.find((tag) => tag === activeQuarter)) {
+					item.classList.remove('sr-only');
+				} else {
+					item.classList.add('sr-only');
+				}
 			} else {
-				return item.classList.add('sr-only');
+				item.classList.add('sr-only');
 			}
-
 		});
 
-		updatePagination(items);
+		updatePagination(allItems);
 
 		function updatePagination(elements) {
 			const filteredArray = [];
 			const paginationButtons = document.querySelectorAll('.js-objects-pagination-button');
-			elements.forEach((element, index) => {
+			elements.forEach((element) => {
 				element.classList.remove('active-item');
 				if (!element.classList.contains('sr-only')) {
 					filteredArray.push(element);
@@ -111,7 +130,7 @@ const objectsFilter = {
 
 		Array.from(objects).map((element, index) => {
 			if (index > condition && index - 1 < condition + 6) {
-			 	element.classList.remove('sr-only');
+				element.classList.remove('sr-only');
 			} else {
 				element.classList.add('sr-only');
 			}
